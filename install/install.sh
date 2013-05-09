3e96280e9f566ede7675e8e7e9f18ad467da0fa0
root <root@Tagada-server.bouyguesbox.fr>
2013-05-09 15:09:17 +0200

    Modification of the install script and required packages. Changing the commit of rivescript to load the init script.

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git submodule update --init
-sudo apt-get install mongodb
-pip -r install Install/requirements.txt
+sudo apt-get install mongodb python-setuptools libxslt1-dev libxslt1.1 libxml2-dev build-essential python-dev
+sudo easy_install pip
+sudo pip install -r install/requirements.txt
