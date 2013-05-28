6da53e7a07aa3d90f0f1079d13044e2d160445ee
Julien Syx <nephilim.seraphin@gmail.com>
2013-05-28 23:28:14 +0200

    listing modules from web app

--- JARVIS-Engine/web/plugins/views.py
+++ JARVIS-Engine/web/plugins/views.py
@@ -2,22 +2,15 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Plugin
 import requests
-import json
+import json, base64
 
 def index(request):
-    modules = []
-    repo = requests.get('https://api.github.com/repos/Seraf/JARVIS-Plugins/contents/')
-    if(repo.ok):
-        repoItems = json.loads(repo.text or repo.content)
-        for item in repoItems:
-            if(     item['type'] == 'dir' or \
-                    (item['type'] == 'file' and 'Seraf/JARVIS-Plugins' not in item['git_url'])):
-                
-                for plugin in Plugin.objects(name=item['name']):
-                    item['enabled'] = plugin.enabled
-                modules.append(item)
-
-    # Get all plugins from DB
-    plugins = Plugin.objects
-    return render_to_response('index.html', {'Plugins': plugins, 'RepoItems': modules},
+    plugins = []
+    metareq = requests.get('https://raw.github.com/Seraf/JARVIS-Plugins/master/plugin_list.json')
+    if(metareq.ok):
+        for item in json.loads(metareq.text or metareq.content):
+            for plugin in Plugin.objects(name=item['name']):
+                item['enabled'] = plugin.enabled
+            plugins.append(item)
+    return render_to_response('index.html', {'Plugins': plugins},
                               context_instance=RequestContext(request))
