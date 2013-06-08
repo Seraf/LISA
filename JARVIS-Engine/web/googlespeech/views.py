985b687198fbcdeb2914fe60ddd23c2d563f8a62
Julien Syx <nephilim.seraphin@gmail.com>
2013-06-08 15:19:10 +0200

    fixing speech view

--- JARVIS-Engine/web/googlespeech/views.py
+++ JARVIS-Engine/web/googlespeech/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'googlespeech/index.html', context)
