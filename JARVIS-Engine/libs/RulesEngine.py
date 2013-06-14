90cf950b9215c5be48869892bd801e28a9b41ebb
Julien Syx <nephilim.seraphin@gmail.com>
2013-06-14 22:17:15 +0200

    Simple update, but now Rules will be done directly with python code

--- JARVIS-Engine/libs/RulesEngine.py
+++ JARVIS-Engine/libs/RulesEngine.py
@@ -12,33 +12,22 @@ class RulesEngine():
         data_defaultanswer =    {
                                     "name": "DefaultAnwser",    \
                                     "order": 999,               \
-                                    "conditions": [
-                                                    {
-                                                        "if": '1',                  \
-                                                        "operator": '==',           \
-                                                        "compare": '1',             \
-                                                    }
-                                                  ],
-                                    "then": "jarvisprotocol.transport.write(json.dumps(                     \
+                                    "code": "jarvisprotocol.transport.write(json.dumps(                     \
                                                     {                                                       \
                                                         'plugin': jsonAnswer['plugin'],                     \
                                                         'method': jsonAnswer['method'],                     \
                                                         'body': jsonAnswer['body'],                         \
                                                         'client_uuid': jarvisprotocol.client_uuid,          \
                                                         'from': jsonData['from']                            \
-                                                    }))",       \
-                                    "end": True
+                                                    }))",
+                                    "end": True,
+                                    "enabled": True
                                 }
         self.database.rules.update(key_defaultanswer, data_defaultanswer, upsert=True)
 
     def Rules(self, jsonData, jsonAnswer, jarvisprotocol):
         rulescollection = self.database.rules
-        for rule in rulescollection.find().sort([("order", 1)]):
-            # should build a if string by recursively parsing conditions
-            # condition = self.build_condition(conditions=rule['conditions'])
-            for condition in rule['conditions']:
-                condition_string = 'if ' + condition['if'] + ' ' + condition['operator'] + ' ' +            \
-                                   condition['compare'] + ': '
-                exec(condition_string + rule['then'])
+        for rule in rulescollection.find({ "enabled": True }).sort([("order", 1)]):
+            exec(rule['code'])
             if rule['end']:
                 break
