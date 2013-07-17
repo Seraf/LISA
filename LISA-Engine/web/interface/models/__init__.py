e4ed8c1081179439fc5511c47e9a658323fdc4b6
Seraf <julien.syx@gmail.com>
2013-07-17 10:49:52 +0200

    Fixing the import for local manage.py

--- LISA-Engine/web/interface/models/__init__.py
+++ LISA-Engine/web/interface/models/__init__.py
@@ -1,2 +1,6 @@
-from web.interface.models.widget import Widget, WidgetUser
-from web.interface.models.workspace import Workspace
+try:
+    from web.interface.models.widget import Widget, WidgetUser
+    from web.interface.models.workspace import Workspace
+except ImportError:
+    from interface.models.widget import Widget, WidgetUser
+    from interface.models.workspace import Workspace
