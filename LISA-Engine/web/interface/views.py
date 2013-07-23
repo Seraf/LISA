2cf498074b52488bd01a503cfad93aed9967ddcc
Seraf <julien.syx@gmail.com>
2013-07-23 11:05:43 +0200

    Adding login required to dashboard

--- LISA-Engine/web/interface/views.py
+++ LISA-Engine/web/interface/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+@login_required()
 def dashboard(request):
     return render(request, 'dashboard.html', {
     })
