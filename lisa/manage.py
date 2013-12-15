6c4826db2c36815d7e6ee432177c1df61abab47a
Julien Syx <julien.syx@gmail.com>
2013-12-15 06:28:28 +0100

    Forgot to update reference for plugins module

--- lisa/manage.py
+++ lisa/manage.py
@@ -5,7 +5,7 @@ from os import environ
 from os.path import dirname, abspath
 import sys
 from django.core.management import execute_from_command_line
-from web import lisa, googlespeech, plugins, interface
+from web import lisa, googlespeech, manageplugins, interface
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.lisa.settings")
