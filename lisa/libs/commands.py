cc6950f438e0822eba27212355a938a61cef1cf1
Julien Syx <julien.syx@gmail.com>
2013-12-28 09:40:57 +0100

    Adding some inline doc to the json of commands, and modifying a little the json sent

--- lisa/libs/commands.py
+++ lisa/libs/commands.py
@@ -4,6 +4,16 @@ from twisted.python.reflect import namedAny
 from twisted.python import log
 
 class Commands():
+    """
+        body : contains the text that will be spoken
+        type : will be "command"
+        from : the message is issued from "LISA Server"
+        command : contains the command name
+        clients_zone : should be ['sender'] to answer to the client who issued the command,
+        * : you can pass other parameters in your json. The client will handle these as he recognize the command name
+
+    """
+
     def __init__(self, configuration, lisaprotocol):
         self.configuration = configuration
         client = MongoClient(configuration['database']['server'], configuration['database']['port'])
@@ -14,8 +24,8 @@ class Commands():
         self.lisaprotocol = lisaprotocol
 
     def mute(self, clientList):
-        #ask clients to mute
-        #
+        # ask clients to mute
+        # TODO should be fixed, it's not good json to send
         if 'all' in clientList:
             for client in self.lisaprotocol.factory.clients:
                 client['object'].sendLine("{'body': 'mute', 'from': 'LISA Server', 'type': 'command'}")
@@ -30,9 +40,10 @@ class Commands():
             self.lisaprotocol.answerToClient(json.dumps(
                 {
                     'body': self._('LOGIN') % (jsonData['from'], jsonData['zone']),
+                    'command': 'LOGIN',
                     'clients_zone': ['sender'],
                     'from': 'LISA Server',
-                    'type': 'chat',
+                    'type': 'command',
                     'bot_name': self.configuration['bot_name']
                 }
             ))
