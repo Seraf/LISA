0795ffe195798461961fc41329fb7df30ec429c3
Julien Syx <julien.syx@gmail.com>
2014-05-13 22:05:37 +0200

    Test with a dedicated plugin now

--- lisa/server/tests/test_plugins.py
+++ lisa/server/tests/test_plugins.py
@@ -8,17 +8,17 @@ class LisaClientTestCase_Plugin(unittest.TestCase):
         self.pluginManager = PluginManagerSingleton.get()
 
     def test_a_install_plugin(self):
-        answer = self.pluginManager.installPlugin(plugin_name="ChatterBot", test_mode=True)
+        answer = self.pluginManager.installPlugin(plugin_name="UnitTest", test_mode=True)
         self.assertEqual(answer['status'], "success")
 
     def test_b_disable_plugin(self):
-        answer = self.pluginManager.disablePlugin(plugin_name="ChatterBot")
+        answer = self.pluginManager.disablePlugin(plugin_name="UnitTest")
         self.assertEqual(answer['status'], "success")
 
     def test_c_enable_plugin(self):
-        answer = self.pluginManager.enablePlugin(plugin_name="ChatterBot")
+        answer = self.pluginManager.enablePlugin(plugin_name="UnitTest")
         self.assertEqual(answer['status'], "success")
 
     def test_d_uninstall_plugin(self):
-        answer = self.pluginManager.uninstallPlugin(plugin_name="ChatterBot")
+        answer = self.pluginManager.uninstallPlugin(plugin_name="UnitTest")
         self.assertEqual(answer['status'], "success")
